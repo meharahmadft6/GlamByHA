df9f1e6c2fd3c4fe15353899e9ed848e1bd1f4dd
Muhammad Ahmad Sadaqat <167222938+meharahmadft6@users.noreply.github.com>
2025-02-19 22:28:31 +0500

    yup

---
+++ app.js
@@ -0,0 +1,22 @@
+const moon = document.querySelector("#moon");
+const body = document.querySelector("body");
+
+// Ensure the dark mode toggle works
+moon.addEventListener("click", () => {
+  body.classList.toggle("dark");
+});
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+});
